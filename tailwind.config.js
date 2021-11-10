f30b25e8052d7342dccb88284083a67fdd84853d
Alex Chang <alexchang42@outlook.com>
2021-11-09 19:35:32 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '960px',
+		},
 		extend: {},
 	},
 	variants: {
