93089af6da6544341625c8c5c02eb7fbeab3d573
Alex Chang <alexchang42@outlook.com>
2021-11-10 14:28:01 -0500

    Update component to render passed props

--- src/components/BioCard.js
+++ src/components/BioCard.js
@@ -1,28 +1,28 @@
 import './BioCard.css';
 import profilePic from '../img/profile.jpeg';
 
-function BioCard() {
+function BioCard(props) {
 	return (
 		<div className="bio-card">
 			<div className="bio-content">
 				<div className="bio-image-wrapper">
-					<img src={profilePic} alt="Profile" />
+					<img src={profilePic} alt="Profile"/>
 				</div>
 				<div className="bio-main">
-					<h1 className="profile-name">Nick Reynolds</h1>
+					<h1 className="profile-name">{props.data.name}</h1>
 					<div className="bio-text-wrapper">
 						<div className="bio-text">
 							<h2 className="subtitle">Bio</h2>
-							<p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+							<p className="description">{props.data.bio}</p>
 						</div>
 						<div className="bio-contact">
 							<div>
 								<h2 className="contact-info-title">Phone</h2>
-								<p className="contact-info-data"></p>
+								<p className="contact-info-data">{props.data.phone}</p>
 							</div>
 							<div>
 								<h2 className="contact-info-title">Email</h2>
-								<p className="contact-info-data"></p>
+								<p className="contact-info-data">{props.data.email}</p>
 							</div>
 						</div>
 					</div>
