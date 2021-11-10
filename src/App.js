48e8f17be2539d07aabf805fd2edebb389c7b3be
Alex Chang <alexchang42@outlook.com>
2021-11-09 23:07:48 -0500

    Update component imports on App.js, add wrapper grid around cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import BioCard from './components/BioCard';
+import AlbumCard from './components/AlbumCard';
 
 function App() {
-  return (
-    <main className="app">
-		<div className="container mx-auto">
+	return (
+	<main className="app">
+		<div className="container mx-auto px-4 lg:px-0 space-y-8">
 			<BioCard></BioCard>
+			<div className="album-card-grid">
+				<AlbumCard></AlbumCard>
+			</div>
 		</div>
-    </main>
-  );
+	</main>
+	);
 }
 
 export default App;
