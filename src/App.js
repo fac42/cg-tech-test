7d99877b1fbdfe98f7ef89a92b798dbd7b3b8d2f
Alex Chang <alexchang42@outlook.com>
2021-11-10 14:28:14 -0500

    Checkpoint commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import React, { useState, useEffect} from 'react';
 import './App.css';
 import BioCard from './components/BioCard';
 import AlbumCard from './components/AlbumCard';
 
 function App() {
+	const [data, setData] = useState();
+	useEffect(() => {
+		const url = "http://localhost:8000/";
+		const fetchData = async () => {
+			try {
+				const response = await fetch(url);
+				const json = await response.json();
+				console.log(data);
+				setData(json);
+
+			}
+			catch (error) {
+				console.log(error);
+			}
+		}
+		fetchData()
+	},[data])
+
 	return (
 	<main className="app">
 		<div className="container mx-auto px-4 lg:px-0 space-y-8">
-			<BioCard></BioCard>
+			<BioCard data={data}></BioCard>
 			<div className="album-card-grid">
-				<AlbumCard isFeatured={true} ></AlbumCard>
+				{data.album.map((item) => {
+					return <AlbumCard data={item}></AlbumCard>
+				})}
 			</div>
 		</div>
 	</main>
--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,27 +1,27 @@
 import './AlbumCard.css';
-import landscapeImageOne from '../img/landscape1.jpeg';
+import img1 from '../img/landscape1.jpeg';
 
 function AlbumCard(props) {
-	let featuredIconClass = props.isFeatured ? 'visible' : 'invisible';
+	let featuredIconCssClass = true ? 'visible' : 'invisible';
 	return(
 		<article className="album-card">
 			<div className="card-upper">
-				<img className="card-image" src={landscapeImageOne} alt="Landscape"/>
+				<img className="card-image" src={img1} alt="Landscape"/>
 				<h2 className="card-title">
-					Card Title
+					{/* {props.data.title} */}
 				</h2>
 			</div>
 			<div className="card-lower">
 				<p className="card-description">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit qui omnis ipsa unde aut eos!
+					{/* {props.data.description} */}
 				</p>
 				<div className="card-metadata">
-					<div className={featuredIconClass}>
+					<div className={featuredIconCssClass}>
 						<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" viewBox="0 0 20 20" fill="currentColor">
 							<path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
 						</svg>
 					</div>
-					<p className="date">2021-11-11</p>
+					{/* <p className="date">{props.data.date}</p> */}
 				</div>
 			</div>
 		</article>
